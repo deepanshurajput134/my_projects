5b8d3bf3ccb570b4b8387c4e5622bff20668d927
deepanshurajput134 <deepanshurajput134@gmail.com>
2025-06-21 17:29:50 +0530

    all of good

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,14 +1,17 @@
-function pick(val){
-    document.getElementById("inputbox").value += val;
+function pick(vale){
+    var a = document.getElementById("inputbox").value += vale;
 }
 
 function clr(){
-    document.getElementById("inputbox").value = "";
+    document.getElementById("inputbox").value = " ";
 }
 
 function solve(){
     let x = document.getElementById("inputbox").value;
     let y = eval(x);
     document.getElementById("inputbox").value = y;
+}
 
+function minus(){
+    document.getElementById("inputbox").value - val;
 }
