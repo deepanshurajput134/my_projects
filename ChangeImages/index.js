94c1e4aa93c42642b75032d577c035440be70098
deepanshurajput134 <deepanshurajput134@gmail.com>
2025-06-22 13:57:57 +0530

    make projects

---
+++ ChangeImages/index.js
@@ -0,0 +1,16 @@
+function first(){
+    var a = document.getElementById("image");
+    a.src = "/src/images/box.avif"
+}
+function second(){
+    var b = document.getElementById("image");
+    b.src = "/src/images/butterfly.avif"
+}
+function third(){
+    var b = document.getElementById("image");
+    b.src = "/src/images/Peacock.avif"
+}
+function fourth(){
+    var b = document.getElementById("image");
+    b.src = "/src/images/Umbrella.avif"
+}
